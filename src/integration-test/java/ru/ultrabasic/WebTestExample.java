a7b35ef0192d34d6e019493a947a347f1d4e20ab
IAmProgrammist <vladislav.pakhomov@bk.ru>
2023-07-20 13:21:45 +0300

    Better project integration

---
+++ src/integration-test/java/ru/ultrabasic/WebTestExample.java
@@ -0,0 +1,21 @@
+package ru.ultrabasic;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class WebTestExample {
+    @BeforeEach
+    public void openPage() {
+        Selenide.open("http://localhost:8080/");
+    }
+
+    @Test
+    public void regularTest() {
+        $("button").shouldHave(Condition.text("Сделать заявление"));
+    }
+}
